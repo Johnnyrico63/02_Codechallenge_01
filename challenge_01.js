a7fdf84c849e29a9ed432b0a1c41f7ce2af4de13
RiKo <ri.ko@outlook.com>
2021-05-18 15:56:00 +0200

    aufgabe gelöst

--- challenge_01.js
+++ challenge_01.js
@@ -8,3 +8,24 @@ Berechnen Sie die Summe der Zahlen.
 Geben Sie die Summe der Zahlen in die Konsole aus:
 ‘Die Summe der Zahlen ist: summe ‘
 */
+let number1, number2, number3;
+number1 = prompt("Bitte eine Zahl eingeben");
+number2 = prompt("Bitte noch eine Zahl eingeben");
+
+/*
+number1 = Number(number1);
+number2 = Number(number2);
+number3 = number1 + number2;
+*/
+
+number3 = +number1 + +number2;
+
+
+//number3 = Number(number1) + Number(number2);
+
+console.log(typeof number1);
+console.log(typeof number2);
+console.log(typeof number3);
+
+
+console.log("Die Summe der Zahlen ist: " + number3 );
